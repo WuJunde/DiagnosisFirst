86410ec90d0fe0d183b22963f1af98c3582bd7bb
wujunde <wujunde@baidu.com>
2021-12-13 15:25:57 +0800

    release0.15

--- README.md
+++ README.md
@@ -1,13 +1,38 @@
+# Opinions Vary? Diagnosis First!
+
+This is a pytorch implementation of paper "Opinions Vary? Diagnosis First!". We propose a novel method to learn the diagnosis-first segmentation from the multiple labeled data. This method beat the popular majority vote by a large margin. 
+
+figure
+
+
+
 ## Preparation
-pytorch1.8.1 + cuda 10.1
+
+The code is run on pytorch1.8.1 + cuda 10.1.
+
 ## Quick Start
-Generate DFSim:
+#### Generate DFSim:
 
 python val.py -net 'your_backbone' -mod val_ad -exp_name generate_dfsim -weights 'weights of diagnosis network'
 
-Train Segmentation:
+#### Train Segmentation:
 
 python train.py -net transunet -mod seg -exp_name repro_seg -base_weights 'weights of diagnosis network'
 
+#### Segmentation Inference:
+
+python val.py -net 'backbone' -mod set -exp_name val_seg -weights 'recorded weights'
+
 See cfg.py for more avaliable parameters
+
+
+
+### Todo list
+
+- [ ] add requirement
+- [x] del debug code
+- [x] cls validation
+- [ ] function name alignment
+- [ ] del trials
+- [ ] dataset preprocess tools
 
--- cfg.py
+++ cfg.py
@@ -1,6 +1,6 @@
 import argparse
 
-def parse_args():    
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-net', type=str, required=True, help='net type')
     parser.add_argument('-baseline', type=str, default='unet', help='baseline net type')
@@ -20,10 +20,10 @@ def parse_args():
     parser.add_argument('-heads', type=int, default=16, help='heads number')
     parser.add_argument('-mlp_dim', type=int, default=1024, help='mlp_dim')
     parser.add_argument('-w', type=int, default=4, help='number of workers for dataloader')
-    parser.add_argument('-b', type=int, default=2, help='batch size for dataloader')
+    parser.add_argument('-b', type=int, default=16, help='batch size for dataloader')
     parser.add_argument('-s', type=bool, default=True, help='whether shuffle the dataset')
     parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
-    parser.add_argument('-lr', type=float, default=3e-4, help='initial learning rate')
+    parser.add_argument('-lr', type=float, default=1e-4, help='initial learning rate')
     parser.add_argument('-imp_lr', type=float, default=3e-4, help='implicit learning rate')
     parser.add_argument('-weights', type=str, default = 0, help='the weights file you want to test')
     parser.add_argument('-base_weights', type=str, default = 0, help='the weights baseline')
@@ -33,7 +33,7 @@ def parse_args():
     type=str,
     default='../dataset',
     help='The path of segmentation data')
-    
+
     opt = parser.parse_args()
 
     return opt
